6b33b2b4e4975c7c87b5154583664d303dcd93eb
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-05-04 07:45:11 -0300

    :rocket: Conectando com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './module/courses/courses.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'docker',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true
+  }),
+  CoursesModule,
+],
   controllers: [AppController],
   providers: [AppService,],
 })
--- src/module/courses/courses.module.ts
+++ src/module/courses/courses.module.ts
@@ -1,8 +1,11 @@
+import { Course } from './entities/course.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesService } from './courses.service';
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Course])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
