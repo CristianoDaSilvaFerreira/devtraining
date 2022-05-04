8ee1fee7b4b1a47eb5f5dd38cf8c585aa2b5a2ef
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-05-04 17:48:07 -0300

    :rocket: Criando a `entity tag`e o relacionamento dela

--- src/module/courses/courses.module.ts
+++ src/module/courses/courses.module.ts
@@ -1,3 +1,4 @@
+import { Tag } from './entities/tag.entity';
 import { Course } from './entities/course.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesService } from './courses.service';
@@ -5,7 +6,7 @@ import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, Tag])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
